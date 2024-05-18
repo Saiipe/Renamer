37667ae2c0ff9fe8028ebd34789d6c6d5f8fd6c9
Tauan <itauandejesus@gmail.com>
2024-05-18 12:43:24 -0300

    logica finalizada
    proximo commit, loop para outros pdf

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
-<h2>Como surgiu a ideia?</h2>
-<p>A ideia surgiu por causa de uma empresa que faz um processo manual de renomear arquivos.</p>
+<h1 style="text-align: center;">Remaner</h1>
+
+<H2>Como surgiu a ideia?</H2>
+<p>A ideia surgiu devido a uma empresa que realiza um processo manual de renomeação de arquivos, um processo bastante demorado e que consome muito tempo para ser concluído. </p>
 
 <h2>Problemas durante o percurso :</h2>
 <p>Extração de dados: No início do projeto, eu não estava conseguindo extrair os dados do PDF. Pensei em transformar os PDF em imagens para facilitar a extração dos dados. Funcionou até certo ponto, mas não consegui progredir devido ao fato de que o código que eu estava escrevendo estava ficando confuso. Então, decidi mudar a abordagem e permitir que a pessoa selecionasse a parte que deveria ser copiada, semelhante a uma ferramenta de captura de tela. Assim, somente os dados daquela região seriam gravados. No entanto, como os PDFs podem variar, essa abordagem não seria eficaz. Abandonei o código por um tempo, mas recentemente retornei e refatorei o código para torná-lo mais organizado e de fácil entendimento, e espero finalizar para colocar em pratica.</p>
 
 <h2>Alternativa usada por enquanto</h2>
 <p>Para facilitar esse processo, usei macros, mas isso me impedia de usar o computador. Embora seja uma boa solução para o problema, configurar o macro para uma pessoa leiga é muito complexo.</p>
+</br>
+<h4>OBS</h4>
+<p>Lembre-se de que o projeto é para um propósito específico. Caso você precise usá-lo em sua empresa, será necessário fazer algumas alterações.</p>
--- Renamer.py
+++ Renamer.py
@@ -1,17 +1,21 @@
 import PyPDF2
 import re
+import os
 
-pdf_path = "SEU PDF 1"
-pdf_path1 = "SEU PDF 2"
-def extrair_textoPdf(pdf_path):
+pdfEspelho = "Seu Espelho"
+pdfBol = "Seu Boleto"
+def extrair_textoPdf(pdf):
     text = ""
-    with open(pdf_path, 'rb') as file:
+    with open(pdf, 'rb') as file:
         pdf_reader = PyPDF2.PdfReader(file)
         num_pages = len(pdf_reader.pages)
         for page_number in range(num_pages):
             page = pdf_reader.pages[page_number]
             text += page.extract_text()
     return text
+
+def formatar_cnpj(cnpj):
+    return f"{cnpj[:2]}.{cnpj[2:5]}.{cnpj[5:8]}/{cnpj[8:12]}-{cnpj[12:14]}"
 
 def extrairValor(text):
 
@@ -31,20 +35,27 @@ def extrairCnpj(text):
      pattern_cnpj = r'\b\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2}\b'
      cnpjs = re.findall(pattern_cnpj, text)
 
-     return cnpjs
+     return cnpjs[:2]
 
-def extrairCnpj1(text):
+def extrairCnpj1(text):# O cnpj abaixo precisa ser formatado!
     pattern_cnpj1 = r'\b\d{2}\d{3}\d{3}\d{4}\d{2}\b'
     cnpjs1 = re.findall(pattern_cnpj1, text)
 
+    cnpjs1 = [formatar_cnpj(cnpj) for cnpj in cnpjs1]
     return cnpjs1
 
-
-
-for cnpj in extrairCnpj(extrair_textoPdf(pdf_path)):
+for cnpj in extrairCnpj1(extrair_textoPdf(pdfEspelho)):
+    print(cnpj + " # Client")
+for cnpj in extrairCnpj(extrair_textoPdf(pdfBol)):
     print(cnpj)
-for cnpj in extrairCnpj1(extrair_textoPdf(pdf_path)):
-    print(cnpj)
-print(extrair_textoPdf(pdf_path))
-#print(extrairValor(extrair_textoPdf(pdf_path)))
-#print(extrairCpf(extrair_textoPdf(pdf_path)))
+
+padrao = r"-d"
+for cpnjC in extrairCnpj1(extrair_textoPdf(pdfEspelho)):
+    for cnpjB in extrairCnpj(extrair_textoPdf(pdfBol)):
+        if (cpnjC == cnpjB):
+            print("sim")
+            bol = pdfBol
+            bolFormatado = re.sub(padrao,"",bol)
+            cpnjC = os.rename(pdfEspelho, "DOC-"+bolFormatado)
+        else:
+            print("Nao")
