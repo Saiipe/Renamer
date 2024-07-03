86ae7b78bbb81476b973bc121177cd8e350762eb
Tauan <itauandejesus@gmail.com>
2024-07-03 08:56:23 -0300

    Update
    tinha criando um metodo e esqueci de incluir

--- Renamer.py
+++ Renamer.py
@@ -3,7 +3,6 @@ import re
 import os
 from tkinter import filedialog, Tk, messagebox
 
-
 def extrair_textoPdf(pdf):
     text = ""
     with open(pdf, 'rb') as file:
@@ -14,22 +13,18 @@ def extrair_textoPdf(pdf):
             text += page.extract_text()
     return text
 
-
 def formatar_cnpj(cnpj):
     return f"{cnpj[:2]}.{cnpj[2:5]}.{cnpj[5:8]}/{cnpj[8:12]}-{cnpj[12:14]}"
-
 
 def extrairValorBol(text):
     pattern = r'Valor do Documento\n(\d{1,3}(?:\.\d{3})*,\d{2})'
     money_values = re.findall(pattern, text)
     return money_values
 
-
 def extrairValorEspelho(text):
     pattern = r'Valor:  (\d{1,3}(?:\.\d{3})*,\d{2})'
     money_values = re.findall(pattern, text)
     return money_values
-
 
 def extrairCpf(text):
     pattern_cpf = r'\.\d{3}\.\b\d{3}\d{3}-\d{2}\b'
@@ -42,12 +37,10 @@ def extrairCpf1(text):
     cpfs = [formatar_cnpj(cpf) for cpf in cpfs]
     return cpfs
 
-
 def extrairCnpj(text):
     pattern_cnpj = r'\b\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2}\b'
     cnpjs = re.findall(pattern_cnpj, text)
     return cnpjs[:2]
-
 
 def extrairCnpj1(text):  # O cnpj abaixo precisa ser formatado!
     pattern_cnpj1 = r'\b\d{2}\d{3}\d{3}\d{4}\d{2}\b'
@@ -61,21 +54,23 @@ tela.withdraw()
 pdfEspelhos = filedialog.askopenfilenames(title="Escolha os espelhos", filetypes=[("PDF files", "*.pdf")])
 pdfBoletos = filedialog.askopenfilenames(title="Escolha os boletos", filetypes=[("PDF files", "*.pdf")])
 
-pattern = re.compile(r"-(d|D|a|A)")
+padrao = re.compile(r"-(d|D|a|A)")
 
 if pdfEspelhos and pdfBoletos:
     for pdfEspelho in pdfEspelhos:
         cpnjsC = extrairCnpj1(extrair_textoPdf(pdfEspelho))
+        cpfC= extrairCpf1(extrair_textoPdf(pdfEspelho))
         valoresEspelho = extrairValorEspelho(extrair_textoPdf(pdfEspelho))
         
         for pdfBol in pdfBoletos:
             cnpjsB = extrairCnpj(extrair_textoPdf(pdfBol))
+            cpfB = extrairCpf(extrair_textoPdf(pdfBol))
             valoresBol = extrairValorBol(extrair_textoPdf(pdfBol))
             
-            if set(cpnjsC).intersection(cnpjsB):
+            if set(cpnjsC).intersection(cnpjsB) or set(cpfC).intersection(cpfB):
                 for valorEspelho in valoresEspelho:
                     if valorEspelho in valoresBol:
-                        bolFormatado = re.sub(pattern, "", os.path.basename(pdfBol))
+                        bolFormatado = re.sub(padrao, "", os.path.basename(pdfBol))
                         novo_nome = f"DOC-{bolFormatado}.pdf"
                         novo_caminho = os.path.join(os.path.dirname(pdfEspelho), novo_nome)
                         
